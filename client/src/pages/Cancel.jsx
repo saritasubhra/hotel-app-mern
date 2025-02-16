738337e405214e72cf17cf25b2b4f82e84d1472e
saritasubhra <saritasubhra1@gmail.com>
2025-02-16 19:27:57 +0530

    cancel page

--- client/src/pages/Cancel.jsx
+++ client/src/pages/Cancel.jsx
@@ -1,5 +1,21 @@
+import { OctagonAlert } from "lucide-react";
+import { Link } from "react-router-dom";
+
 function Cancel() {
-  return <div>Cancel</div>;
+  return (
+    <div className="flex flex-col gap-4 capitalize justify-center items-center py-20 ">
+      <p className="text-xl font-thin">something went wrong</p>
+      <p className="text-3xl font-bold">Please try again!</p>
+      <p>
+        <OctagonAlert color="#b00303" size={50} />
+      </p>
+      <p>
+        <Link to="/">
+          <button className="btn-black bg-[#b00303]">← Go Back</button>
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Cancel;
